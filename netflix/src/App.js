ca64dc9955d7e726650c770ecc1796088e8a984d
Miley Wright <dishaliekwright@yahoo.com>
2020-10-14 00:24:38 -0400

    JumboTron complete

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -8,10 +8,13 @@ function App() {
       {JumboData.map(item => {
         return (
         <Jumbotron key={item.id} direction={item.direction} >
-          <p>{item.title}</p>
-          <p>{item.subTitle}</p>
-          <p>{item.image}</p>
-          <p>{item.alt}</p>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron.Pane>
         </Jumbotron>  
       )})}
     </Jumbotron.Container>
