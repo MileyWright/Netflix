28f61fbadecd13492bcf644e491c34de771266b9
Miley Wright <dishaliekwright@yahoo.com>
2020-10-16 02:13:49 -0400

    routes set up - browse, signin, signup

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {Home, Browse, Signin, Signup } from './Pages';
 import * as Routes from './Constants/routes';
-import Home from './Pages/home';
+
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path={Routes.HOME} component={Home} />
+        <Route exact path={Routes.BROWSE} component={Browse} />
+        <Route exact path={Routes.SIGN_IN} component={Signin} />
+        <Route exact path={Routes.SIGN_UP} component={Signup} />
       </Switch>
     </Router>
   );
