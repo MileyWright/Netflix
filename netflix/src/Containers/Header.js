319a0915c215b131f5729c9f7558f0909a2baeb6
Miley Wright <dishaliekwright@yahoo.com>
2020-10-19 14:07:06 -0400

    Logo and sign in button

--- netflix/src/Containers/Header.js
+++ netflix/src/Containers/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Header } from '../Components'
-
+import * as Routes from '../Constants/routes';
+import logo from '../logo.svg';
 export default function HeaderContainer({children}) {
     return (
         <Header>
             <Header.Frame>
-                <Header.Logo/>
-                <Header.Button />
+                <Header.Logo to={Routes.HOME} src={logo} alt='Netflix' />
+                <Header.Button to={Routes.SIGN_IN}>Sign In</Header.Button>
             </Header.Frame>
             {children}
         </Header>
