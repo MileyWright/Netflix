23a98715a709f43de65aecb4b3478d6b52e892e4
Miley Wright <dishaliekwright@yahoo.com>
2020-10-19 22:51:48 -0400

    signin Styled

--- netflix/src/Components/Form/styles/Form.js
+++ netflix/src/Components/Form/styles/Form.js
@@ -15,25 +15,79 @@ export const Container = styled.div`
     margin-bottom: 100px;
   `;
 
-export const Error = styled.div``;
+export const Error = styled.div`
+    background: #e87c03;
+    border-radius: 4px;
+    font-size: 14px;
+    margin: 0 0 16px;
+    color: white;
+    padding: 15px 20px;
+`;
 
-export const Base = styled.form``;
+export const Base = styled.form`
+    display: flex;
+    flex-direction: column;
+    max-width: 450px;
+    width: 100%;
+`;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+    color: #fff;
+    font-size: 32px;
+    font-weight: bold;
+    margin-bottom: 28px;
+`;
 
-export const Text = styled.p``;
+export const Text = styled.p`
+    color: #737373;
+    font-size: 16px;
+    font-weight: 500;
+`;
 
-export const TextSmall = styled.p``;
+export const TextSmall = styled.p`
+    margin-top: 10px;
+    font-size: 13px;
+    line-height: normal;
+    color: #8c8c8c;
+`;
 
 export const Button = styled(Link)`
     color: #fff;
     text-decoration: none;
+
     &:hover {
     text-decoration: underline;
     }
 `;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+    background: #333;
+    border-radius: 4px;
+    border: 0;
+    color: #fff;
+    height: 50px;
+    line-height: 50px;
+    padding: 5px 20px;
+    margin-bottom: 20px;
 
-export const Submit = styled.button``;
+    &:last-of-type {
+    margin-bottom: 30px;
+    }
+`;
+
+export const Submit = styled.button`
+    background: #e50914;
+    border-radius: 4px;
+    font-size: 16px;
+    font-weight: bold;
+    margin: 24px 0 12px;
+    padding: 16px;
+    border: 0;
+    color: white;
+    cursor: pointer;
+    
+    &:disabled {
+    opacity: 0.5;
+    }
+`;
 
