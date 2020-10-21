9a6ff2b68d1de9fed0e764306132bab284428816
Miley Wright <dishaliekwright@yahoo.com>
2020-10-20 21:39:38 -0400

    pulling movie/show data

--- netflix/src/Pages/Browse.js
+++ netflix/src/Pages/Browse.js
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { useContent } from '../Hooks';
 export default function Browse() {
     // we need the series and the films
+    const { series } = useContent('series');
+    const { films } = useContent('films');
+
     // we need slides
     // pass it to the browse container
     return (
