e15ad3a015032e722be9a6a3bbaad6793293ea3a
Miley Wright <dishaliekwright@yahoo.com>
2020-10-20 23:27:02 -0400

    profile image w/ current user

--- netflix/src/Containers/Browse.js
+++ netflix/src/Containers/Browse.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { SelectProfileContainer } from './Profiles';
+import { FirebaseContext } from '../Context/firebase';
 
 export function BrowseContainer({ slides }) {
+    const { firebase } = useContext(FirebaseContext);
+    const user = firebase.auth().currentUser || {};
     return (
-        <SelectProfileContainer/>
+        <SelectProfileContainer user={user} />
     )
 };
--- netflix/src/Containers/Profiles.js
+++ netflix/src/Containers/Profiles.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from '../Components';
+import { Header, Profiles } from '../Components';
 import * as Routes from '../Constants/routes';
 import logo from '../logo.svg';
 
@@ -11,6 +11,16 @@ export function SelectProfileContainer({ user, setProfile }) {
                     <Header.Logo to={Routes.HOME} src={logo} alt='Netflix' />
                 </Header.Frame>
             </Header>
+            
+            <Profiles>
+                <Profiles.Title>Who's Watching</Profiles.Title>
+                <Profiles.List>
+                    <Profiles.User>
+                        <Profiles.Picture src={user.photoURL} />
+                        <Profiles.Name> {user.displayName}</Profiles.Name>
+                    </Profiles.User>
+                </Profiles.List>
+            </Profiles>
         </>
     )
 }
