17b465f0aa999235a09bd827070e0e7a8ab6abb2
Miley Wright <dishaliekwright@yahoo.com>
2020-10-20 22:02:36 -0400

    series/films data filtered

--- netflix/src/Pages/Browse.js
+++ netflix/src/Pages/Browse.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useContent } from '../Hooks';
+import  selectionFilter from '../Utils/SelectionFilter';
 export default function Browse() {
     // we need the series and the films
     const { series } = useContent('series');
     const { films } = useContent('films');
+    const slides = selectionFilter({ series, films });
 
     // we need slides
     // pass it to the browse container
